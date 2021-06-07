b841348d9a7d97155c4fdb4e9aefe2774afcb1af
thatslucio <luciococorullo17@gmail.com>
2021-06-07 15:55:57 +0200

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
-
 import 'ui/pages/Homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
+}
+
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text("Error");
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Homepage();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text("Loading");
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +47,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Study Buddy',
-      home: Homepage(),
+      home: App(),
     );
   }
 }
